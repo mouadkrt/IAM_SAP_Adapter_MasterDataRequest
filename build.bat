50f30a668f8df4040367dbec3569012dddae50ed
Sentissi Mouad <m.sentissi@gmail.com>
2023-12-01 11:39:23 +0100

    new tag 0.7

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ mvn clean install
 or 
 mvn dependency:purge-local-repository clean install -U
 
-docker build -t muis-fuse-sap-adapter:iam_0.6.1 .
+docker build -t muis-fuse-sap-adapter:iam_0.7 .
 
-docker tag muis-fuse-sap-adapter:iam_0.6.1 quay.io/msentissi/muis-fuse-sap-adapter:iam_0.6.1
+docker tag muis-fuse-sap-adapter:iam_0.7 quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7
 
-docker push quay.io/msentissi/muis-fuse-sap-adapter:iam_0.6.1
+docker push quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7
