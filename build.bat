1207af7ff5e779eab2c10f0d9206357185952cc7
Sentissi Mouad <m.sentissi@gmail.com>
2024-02-13 10:48:45 +0100

    iam_0.7.6

--- build.bat
+++ build.bat
@@ -1,9 +1,5 @@
+REM Ctrl-A, Ctrl-C, Ctrl-V into a terminal
 mvn clean install
-or 
-mvn dependency:purge-local-repository clean install -U
-
-docker build -t muis-fuse-sap-adapter:iam_0.7.4 .
-
-docker tag muis-fuse-sap-adapter:iam_0.7.4 quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7.4
-
-docker push quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7.4
+docker build -t muis-fuse-sap-adapter:iam_0.7.6 .
+docker tag muis-fuse-sap-adapter:iam_0.7.6 quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7.6
+docker push quay.io/msentissi/muis-fuse-sap-adapter:iam_0.7.6
